f8869bcd6dd992128947d611f72147d25a833f06
dpb15 <brannerchinese@gmail.com>
2014-05-25 17:39:29 -0400

    finish asst 1, part 1, questions 1-3

--- server_01.py
+++ server_01.py
@@ -3,7 +3,28 @@
 # David Prager Branner
 # 20140525
 
-"""Construct simple web server."""
+"""Construct simple web server.
+
+Assignments:
+
+ 1.     Using your language's http library, make a server that listens on a
+certain port (say 1924) and returns a string of text.  Check that your browser
+displays that text when you go to localhost:1924.
+
+ 2.  Instead of fixed text, have your program look for a file called index.html
+in its current directory.  Return its contents, or a 404 error if the file
+doesn't exist.
+
+ 3.  Read the url the browser sends and serve the appropriate file - for
+example, localhost:1924/foo.html should look for foo.html in the current
+directory.
+
+Usage:
+
+    python server_01.py 1234
+
+where 1234 is a port to listen on. If no port is found, the default is 8000.
+"""
 
 import sys
 if sys.version_info[0] != 3:
@@ -16,21 +37,22 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class CustomHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header('Content-type', 'text-html')
         self.end_headers()
-        files = 'files'
+        files = 'files/'
         file_contents = None
+        if self.path == '/':
+            self.path = 'index.html'
         if self.path.endswith('.html'):
-            print(self.path)
             try:
-                with open (files + self.path) as f:
+                with open (files + self.path, 'rb') as f:
                     file_contents = f.read()
             except IOError:
-                self.send_error(404, 'file not found!')
-                file_contents = b'You should have received an error.'
+                self.send_error(404, 'File {} not found'.format(self.path))
         else:
-            file_contents = b'Nothing received.'
+            file_contents = b'Path must end with ".html"; try again.'
         if file_contents:
+            # self.wfile is a socket.SocketIO object
             self.wfile.write(file_contents)
 
 def run(port):
